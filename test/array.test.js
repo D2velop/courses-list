fc3f12a6963959dc0ffb94a5ff04cb20ec3bd3ad
cegerard <git@eriatand.org>
2021-03-20 05:22:49 +0100

    rename array import in test

--- test/array.test.js
+++ test/array.test.js
@@ -1,4 +1,4 @@
-import * as arrayList from "../challenge/array";
+import * as arrayService from "../challenge/array";
 
 describe("Array list", () => {
   const firstElement = { label: "ten", weight: 23 };
@@ -20,103 +20,103 @@ describe("Array list", () => {
 
   describe("build array function", () => {
     it("should build a new object", () => {
-      const builtArray = arrayList.buildArray("./data/1K.json");
+      const builtArray = arrayService.buildArray("./data/1K.json");
       expect(typeof builtArray === "object").toBe(true);
     });
 
     it("should build a new array with length", () => {
-      const builtArray = arrayList.buildArray("./data/1K.json");
+      const builtArray = arrayService.buildArray("./data/1K.json");
       expect(builtArray.length).toEqual(1000);
     });
 
     it("should build a new array with well formated element", () => {
-      const builtArray = arrayList.buildArray("./data/1K.json");
+      const builtArray = arrayService.buildArray("./data/1K.json");
       expect(builtArray[0]).toEqual({ weight: 32, label: "nine" });
     });
   });
 
   describe("search one from label", () => {
     it("should return an existing element", () => {
-      expect(arrayList.searchOneFromLabel(arrayFixture, "two")).toEqual(
+      expect(arrayService.searchOneFromLabel(arrayFixture, "two")).toEqual(
         secondElement
       );
     });
 
     it("should return null if no element was found", () => {
       expect(
-        arrayList.searchOneFromLabel(arrayFixture, "not exist")
+        arrayService.searchOneFromLabel(arrayFixture, "not exist")
       ).toBeNull();
     });
   });
 
   describe("search nth from weight", () => {
     it("should return an existing element", () => {
-      expect(arrayList.searchNthFromWeight(arrayFixture, 23, 2)).toEqual(
+      expect(arrayService.searchNthFromWeight(arrayFixture, 23, 2)).toEqual(
         lastElement
       );
     });
 
     it("should return null if not enough elements", () => {
-      expect(arrayList.searchNthFromWeight(arrayFixture, 23, 3)).toBeNull();
+      expect(arrayService.searchNthFromWeight(arrayFixture, 23, 3)).toBeNull();
     });
 
     it("should return null if element does not exist", () => {
-      expect(arrayList.searchNthFromWeight(arrayFixture, 0, 1)).toBeNull();
+      expect(arrayService.searchNthFromWeight(arrayFixture, 0, 1)).toBeNull();
     });
   });
 
   describe("search last from weight", () => {
     it("should return the second(last) occurence of weight 23", () => {
-      expect(arrayList.searchLastFromWeight(arrayFixture, 23)).toEqual(
+      expect(arrayService.searchLastFromWeight(arrayFixture, 23)).toEqual(
         lastElement
       );
     });
 
     it("should return the only occurence of weight 1", () => {
-      expect(arrayList.searchLastFromWeight(arrayFixture, 1)).toEqual(
+      expect(arrayService.searchLastFromWeight(arrayFixture, 1)).toEqual(
         secondElement
       );
     });
 
     it("should return null if no element was found", () => {
-      expect(arrayList.searchLastFromWeight(arrayFixture, 42)).toBeNull();
+      expect(arrayService.searchLastFromWeight(arrayFixture, 42)).toBeNull();
     });
   });
 
   describe("search all from weight", () => {
     it("should return all occurences of weight 23", () => {
-      expect(arrayList.searchAllFromWeight(arrayFixture, 23)).toEqual([
+      expect(arrayService.searchAllFromWeight(arrayFixture, 23)).toEqual([
         firstElement,
         lastElement,
       ]);
     });
 
     it("should return the only occurence of weight 1", () => {
-      expect(arrayList.searchAllFromWeight(arrayFixture, 1)).toEqual([
+      expect(arrayService.searchAllFromWeight(arrayFixture, 1)).toEqual([
         secondElement,
       ]);
     });
 
     it("should return an empty array if no element was found", () => {
-      expect(arrayList.searchAllFromWeight(arrayFixture, 42)).toEqual([]);
+      expect(arrayService.searchAllFromWeight(arrayFixture, 42)).toEqual([]);
     });
   });
 
   describe("Get the nth element", () => {
     it("should return the existing element", () => {
-      expect(arrayList.searchNthElement(arrayFixture, 2)).toEqual(
+      expect(arrayService.searchNthElement(arrayFixture, 2)).toEqual(
         secondElement
       );
     });
 
     it("should return null if no element was found", () => {
-      expect(arrayList.searchNthElement(arrayFixture, 42)).toBeNull();
+      expect(arrayService.searchNthElement(arrayFixture, 42)).toBeNull();
     });
   });
 
   describe("Sort an array based on weight value", () => {
     it("should sort the full array", () => {
-      arrayList.sortOnWeight(arrayFixture);
+      arrayService.sortOnWeight(arrayFixture);
 
       expect(arrayFixture).toEqual([
         { label: "two", weight: 1 },
@@ -134,7 +134,7 @@ describe("Array list", () => {
         { label: "four", weight: 1 },
       ];
 
-      arrayList.sortOnWeight(twoElemArray);
+      arrayService.sortOnWeight(twoElemArray);
 
       expect(twoElemArray).toEqual([
         { label: "four", weight: 1 },
@@ -144,7 +144,7 @@ describe("Array list", () => {
 
     it("should sort an empty array", () => {
       const emptyArray = [];
-      arrayList.sortOnWeight(emptyArray)
+      arrayService.sortOnWeight(emptyArray)
 
       expect(emptyArray).toEqual([]);
     });
@@ -152,7 +152,7 @@ describe("Array list", () => {
 
   describe("Sort an array based on weight value and at weight egual sort on label", () => {
     it("should sort the full array", () => {
-      arrayList.sortOnWeightAndLabel(arrayFixture);
+      arrayService.sortOnWeightAndLabel(arrayFixture);
 
       expect(arrayFixture).toEqual([
         { label: "two", weight: 1 },
@@ -169,7 +169,7 @@ describe("Array list", () => {
         { label: "two", weight: 1 },
         { label: "four", weight: 1 },
       ];
-      arrayList.sortOnWeightAndLabel(twoElemArray);
+      arrayService.sortOnWeightAndLabel(twoElemArray);
 
       expect(twoElemArray).toEqual([
         { label: "four", weight: 1 },
@@ -179,7 +179,7 @@ describe("Array list", () => {
 
     it("should sort an empty array", () => {
       const emptyArray = [];
-      arrayList.sortOnWeightAndLabel(emptyArray)
+      arrayService.sortOnWeightAndLabel(emptyArray)
 
       expect(emptyArray).toEqual([]);
     });
@@ -187,20 +187,20 @@ describe("Array list", () => {
 
   describe("Insert a new element at first position in the array", () => {
     it("should insert the new element at first position", () => {
-      arrayList.insertFirst(arrayFixture, "five", 5);
+      arrayService.insertFirst(arrayFixture, "five", 5);
       expect(arrayFixture[0]).toEqual({ label: "five", weight: 5 });
     });
 
     it("should change the length of the array", () => {
-      const lengthBefore = arrarrayFixtureayList.length;
-      arrayList.insertFirst(arrayFixture, "five", 5);
+      const lengthBefore = arrayFixture.length;
+      arrayService.insertFirst(arrayFixture, "five", 5);
       expect(arrayFixture.length).toEqual(lengthBefore + 1);
     });
   });
 
   describe("Insert a new element at last position in the array", () => {
     it("should insert the new element at last position", () => {
-      arrayList.insertEnd(arrayFixture, "five", 5);
+      arrayService.insertEnd(arrayFixture, "five", 5);
       expect(arrayFixture[arrayFixture.length - 1]).toEqual({
         label: "five",
         weight: 5,
@@ -209,52 +209,52 @@ describe("Array list", () => {
 
     it("should change the length of the array", () => {
       const lengthBefore = arrayFixture.length;
-      arrayList.insertEnd(arrayFixture, "five", 5);
+      arrayService.insertEnd(arrayFixture, "five", 5);
       expect(arrayFixture.length).toEqual(lengthBefore + 1);
     });
   });
 
   describe("Insert a new element at nth position in the array", () => {
     it("should insert the new element at the nth position", () => {
-      arrayList.insertNth(arrayFixture, "five", 5, 3);
+      arrayService.insertNth(arrayFixture, "five", 5, 3);
       expect(arrayFixture[2]).toEqual({ label: "five", weight: 5 });
     });
 
     it("should change the length of the array", () => {
       const lengthBefore = arrayFixture.length;
-      arrayList.insertNth(arrayFixture, "five", 5, 3);
+      arrayService.insertNth(arrayFixture, "five", 5, 3);
       expect(arrayFixture.length).toEqual(lengthBefore + 1);
     });
   });
 
   describe("Remove an element from its weight and label", () => {
     it("should return the former position of the remove element", () => {
-      expect(arrayList.removeFirst(arrayFixture, "four", 30)).toEqual(3);
+      expect(arrayService.removeFirst(arrayFixture, "four", 30)).toEqual(3);
     });
 
     it("should return -1 if no element was removed", () => {
-      expect(arrayList.removeFirst(arrayFixture, "not exist", 99)).toEqual(-1);
+      expect(arrayService.removeFirst(arrayFixture, "not exist", 99)).toEqual(-1);
     });
 
     it("should change the length of the array", () => {
       const lengthBefore = arrayFixture.length;
-      arrayList.removeFirst(arrayFixture, "four", 30);
+      arrayService.removeFirst(arrayFixture, "four", 30);
       expect(arrayFixture.length).toEqual(lengthBefore - 1);
     });
   });
 
   describe("Remove all elements from theire weight", () => {
     it("should return the number of removed elements", () => {
-      expect(arrayList.removeAll(arrayFixture, 23)).toEqual(2);
+      expect(arrayService.removeAll(arrayFixture, 23)).toEqual(2);
     });
 
     it("should return 0 if no element was removed", () => {
-      expect(arrayList.removeAll(arrayFixture, 99)).toEqual(0);
+      expect(arrayService.removeAll(arrayFixture, 99)).toEqual(0);
     });
 
     it("should remove targeted element", () => {
       const weightToRemove = 23;
-      arrayList.removeAll(arrayFixture, weightToRemove)
+      arrayService.removeAll(arrayFixture, weightToRemove)
       expect(arrayFixture.findIndex((elem) => {
         return elem.weight === weightToRemove
       })).toEqual(-1);
@@ -262,7 +262,7 @@ describe("Array list", () => {
 
     it("should change the length of the array", () => {
       const lengthBefore = arrayFixture.length;
-      const nbRemoved = arrayList.removeAll(arrayFixture, 23);
+      const nbRemoved = arrayService.removeAll(arrayFixture, 23);
       expect(arrayFixture.length).toEqual(lengthBefore - nbRemoved);
     });
   });
